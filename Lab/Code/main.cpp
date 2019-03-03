21b06767756dbe27306e0a89d752d505227abb54
MirageLyu <1137935006@qq.com>
2019-03-02 19:28:26 -0800

    Finish Scanner

---
+++ Lab/Code/main.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<cstdio>
+#include<cstring>
+
+using namespace std;
+
+extern FILE* yyin;
+extern int yylex();
+
+
+int main(int argc, char** argv){
+	if(argc > 1){
+		if(!(yyin = fopen(argv[1], "r"))){
+			perror(argv[1]);
+			return 1;
+		}
+	}
+
+	while(yylex() != 0);
+
+	return 0;
+}
